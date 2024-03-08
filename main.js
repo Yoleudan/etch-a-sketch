99dfd2b6f89710ebe6f967dbe9bcb6a47a1542e5
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-03-08 15:07:04 +0000

    Add function for measurement of each square

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const grid = document.querySelector('#grid');
 
+let dimensions = 16
+
+function getSquareWidth(){
+    let width = 960 / dimensions
+    return width + "px";
+}
+
 
 function  makeSquare(){
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.outline = "thin solid grey"
-    square.style.minHeight = "32px"
-    square.style.minWidth = "32px"
+    square.style.minHeight = getSquareWidth();
+    square.style.minWidth = getSquareWidth();
     grid.appendChild(square);
 }
 
-let dimensions = 16
 
 function makeGrid(){
     for (let i = 1; i <= (dimensions*dimensions); i++ ){
